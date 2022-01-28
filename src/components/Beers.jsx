278549c7757b9c8fb33aec3fe14604241359b83b
Victor Giron <victor.giron@blackhill.dev>
2022-01-28 17:59:03 +0100

    with the list of beers

--- src/components/Beers.jsx
+++ src/components/Beers.jsx
@@ -1,9 +1,44 @@
-import React from "react"
-import {Link} from "react-router-dom"
+import axios from "axios";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-function Beers(){
-    
-    return  (<></>)
+function Beers() {
+  const [list, setList] = React.useState([]);
+
+  async function getBeers() {
+    try {
+      const res = await axios.get("https://ih-beers-api2.herokuapp.com/beers");
+      //console.log(theList.data)
+      return res.data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  async function fetchList() {
+    try {
+      setList(await getBeers());
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  console.log(list);
+
+  return (
+    <>
+      <div className="fulllist">
+        <div className="one-beer">
+          {list.map((beer) => (
+            <li key={beer._id}>{beer.name}</li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Beers
+export default Beers;
